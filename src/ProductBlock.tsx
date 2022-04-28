b09a451a54aab5bd1b9c5ca293b45972a9ac1506
Игорь Бахтин <mikage345@gmail.com>
2022-04-28 18:28:59 +0500

    upd

--- src/ProductBlock.tsx
+++ src/ProductBlock.tsx
@@ -1,13 +1,13 @@
-import {JSX} from '@redneckz/uni-jsx';
-import {PrimaryButton} from './ui-kit'
-import {Picture} from "./types";
-import {Icons} from "./Icons";
-import {Img} from "./Img";
+import { JSX } from '@redneckz/uni-jsx';
+import { PrimaryButton } from './ui-kit';
+import { Picture } from './types';
+import { Icons } from './Icons';
+import { Img } from './Img';
 
 export interface Benefit {
-  label: string,
-  description?: string,
-  icon?: keyof typeof Icons
+  label: string;
+  description?: string;
+  icon?: keyof typeof Icons;
 }
 
 export interface ProductBlockContent {
@@ -15,53 +15,69 @@ export interface ProductBlockContent {
   description?: string | Node;
   image?: Picture;
   benefits?: Benefit[];
-  buttonText?: string,
+  buttonText?: string;
   href?: string;
   target?: '_blank' | '_self' | '_parent' | '_top' | string;
 }
 
 export interface ProductBlockProps extends ProductBlockContent {
-  className?: string,
+  className?: string;
 }
 
-export const ProductBlock = JSX<ProductBlockProps>(props => {
-  const {className, title, description, benefits, buttonText = 'Оформить карту', href, target, image} = props;
+export const ProductBlock = JSX<ProductBlockProps>((props) => {
+  const {
+    className,
+    title,
+    description,
+    benefits,
+    buttonText = 'Оформить карту',
+    href,
+    target,
+    image,
+  } = props;
   return (
-    <section className={`bg-white rounded-[40px] h-[470px] flex relative overflow-hidden ${className}`}>
-      <div className={['flex', 'flex-col', 'p-[52px]', image && 'pr-0']
-        .filter(Boolean)
-        .join(' ')}>
-        {title && <h1 className="font-sans font-medium text-[40px] leading-[48px] m-0 mb-[16px] whitespace-pre-wrap">
-          {title}
-        </h1>}
-        {description &&
-          <div className="font-sans font-normal text-[16px] leading-[24px] text-second-primary mb-[28px]">{description}</div>}
-        {benefits && <div className="flex gap-[25px]">
-          {benefits.map((benefit, index) => <div key={index} className="flex gap-[16px] items-center">
-            {benefit.icon &&
-              <div className="h-[48px] w-[48px] min-w-[48px] min-h-[48px] bg-second-light rounded-full p-[10px] box-border">
-                {Icons[benefit.icon]}
-              </div>}
-            <div className="flex gap-[4px] flex-col">
-              <h4 className="font-sans font-medium text-[20px] leading-[28px] m-0">
-                {benefit.label}
-              </h4>
-              {benefit.description &&
-                <div className="font-sans font-normal text-[14px] leading-[20px] text-second-primary">
-                  {benefit.description}
-                </div>}
-            </div>
-          </div>)}
-        </div>}
+    <section className={`bg-white rounded-10 h-[470px] flex relative overflow-hidden ${className}`}>
+      <div className={['flex', 'flex-col', 'p-11', image && 'pr-0'].filter(Boolean).join(' ')}>
+        {title && (
+          <h1 className="font-sans font-medium text-10 leading-11 m-0 mb-4 whitespace-pre-wrap">
+            {title}
+          </h1>
+        )}
+        {description && (
+          <div className="font-sans font-normal text-[16px] leading-6 text-second-primary mb-7">
+            {description}
+          </div>
+        )}
+        {benefits && <div className="flex gap-6">{benefits.map(renderBenefit)}</div>}
         <div className="mt-auto">
-          {href && <PrimaryButton target={target} href={href}>
-            {buttonText}
-          </PrimaryButton>}
+          {href && (
+            <PrimaryButton target={target} href={href}>
+              {buttonText}
+            </PrimaryButton>
+          )}
         </div>
       </div>
-      {image && <div className="h-full max-w-[450px] flex">
-        {<Img image={image} className="flex" />}
-      </div>}
+      {image && <div className="h-full flex">{<Img image={image} className="flex" />}</div>}
     </section>
   );
 });
+
+function renderBenefit(benefit, index) {
+  return (
+    <div key={index} className="flex gap-4 items-center">
+      {benefit.icon && (
+        <div className="h-11 w-11 min-w-11 min-h-11 bg-second-light rounded-full p-2.5 box-border">
+          {Icons[benefit.icon]}
+        </div>
+      )}
+      <div className="flex gap-1 flex-col">
+        <h4 className="font-sans font-medium text-5 leading-7 m-0">{benefit.label}</h4>
+        {benefit.description && (
+          <div className="font-sans font-normal text-3.5 leading-5 text-second-primary">
+            {benefit.description}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,21 @@ module.exports = {
         'second-primary': '#7d7e7f',
         'second-light': '#f5f7f9',
       },
+      minHeight: (theme) => ({
+        ...theme('spacing'),
+      }),
+      minWidth: (theme) => ({
+        ...theme('spacing'),
+      }),
+      fontSize: (theme) => ({
+        ...theme('spacing'),
+      }),
+      paddingSize: (theme) => ({
+        ...theme('spacing'),
+      }),
+      borderRadius: (theme) => ({
+        ...theme('spacing'),
+      }),
     },
   },
   plugins: [],
