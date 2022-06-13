2df23f034f7dbe41e9e468ad0c15e7e5185a493a
redneckz <vasyaselsky@yandex.ru>
2022-06-13 10:04:17 +0300

    Adjust accessibility

--- src/Footer/SocialMedia.tsx
+++ src/Footer/SocialMedia.tsx
@@ -4,9 +4,9 @@ import { TelegramIcon, VKIcon, OkIcon } from '../Icons/index';
 import { useLink } from '../useLink';
 
 const ICONS_MAP = [
-  { origins: ['t.me', 'telegram.org'], icon: TelegramIcon },
-  { origins: ['vk.com'], icon: VKIcon },
-  { origins: ['ok.ru'], icon: OkIcon },
+  { origins: ['t.me', 'telegram.org'], icon: TelegramIcon, label: 'Телеграм' },
+  { origins: ['vk.com'], icon: VKIcon, label: 'ВКонтакте' },
+  { origins: ['ok.ru'], icon: OkIcon, label: 'Одноклассники' },
 ];
 
 export interface SocialMediaProps extends UniBlockProps {
@@ -30,14 +30,15 @@ interface MediaButtonProps {
 }
 
 const MediaButton = JSX<MediaButtonProps>(({ href }) => {
-  const { icon: Icon } =
+  const { icon: Icon, label } =
     ICONS_MAP.find(({ origins }) => origins.some((_) => href?.includes(_))) || {};
   return (
     <a
-      className="flex items-center justify-center border-solid border-1 border-main-divider rounded-full no-underline outline-none w-8 h-8 gap-2"
+      className="flex items-center justify-center border-solid border-1 border-main-divider rounded-full no-underline outline-none w-8 h-8"
       href={href}
       target="_blank"
       rel="noopener noreferrer"
+      aria-label={label}
     >
       {Icon && <Icon className="block" />}
     </a>
--- src/ProductGallery.tsx
+++ src/ProductGallery.tsx
@@ -31,6 +31,7 @@ export const ProductGallery = JSX<ProductGalleryProps>(
         <div
           className={`flex transition duration-1000`}
           style={{ transform: `translateX(-${activeSlideIndex}00%)` }}
+          role="list"
         >
           {galleryBlocks.map((_, i) => renderProductBlock(_, i, context))}
         </div>
@@ -55,7 +56,7 @@ export const ProductGallery = JSX<ProductGalleryProps>(
 
 function renderProductBlock(block: ProductBlockInnerContent, i: number, context) {
   return (
-    <section key={String(i)} className="grow-0 shrink-0 basis-full" role="list">
+    <section key={String(i)} className="grow-0 shrink-0 basis-full" role="listitem">
       <div className="p-11 pr-[7.5rem] ">
         <ProductBlockInner context={context} {...block} />
       </div>
