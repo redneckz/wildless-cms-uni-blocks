d85386fb877310af1753ec2f17b29078658c01e7
andreySimonenko <a.dev.krd@gmail.com>
2022-07-01 09:50:17 +0300

    PromoTile: add fixture (#54)

---
+++ src/PromoTile.fixture.tsx
@@ -0,0 +1,25 @@
+import { context } from './setup-fixture';
+
+import { PromoTile } from './PromoTile';
+
+export default {
+  default: (
+    <div className="grid grid-cols-12">
+      <PromoTile
+        context={context}
+        className="col-span-4"
+        title="Акции и спецпредложения"
+        description="Пенсионный кредит по специальной процентной ставке"
+        version="secondary"
+        date="2022-04-16"
+        buttons={[
+          {
+            text: 'Все акции',
+            href: '/credits',
+            version: 'secondary',
+          },
+        ]}
+      />
+    </div>
+  ),
+};
