0522e25f5bf8e90f581af2cdace929fa6efa524d
vladeiko <vladeiko999@gmail.com>
2022-07-05 16:38:41 +0300

    Refactor InputRange, remove fixed width

--- src/ui-kit/InputRange.tsx
+++ src/ui-kit/InputRange.tsx
@@ -25,7 +25,7 @@ export const InputRange = JSX<InputRangeProps>(
     };
 
     return (
-      <div className={`max-w-[471px] ${className}`}>
+      <div className={className}>
         <div className="relative">
           {title ? (
             <span className="absolute text-xs text-secondary-text top-[6px] pl-[18px]">
@@ -34,21 +34,23 @@ export const InputRange = JSX<InputRangeProps>(
           ) : null}
           <input
             className={`font-sans text-sm w-full h-12 border-2 border-solid border-main-divider rounded-md outline-none p-0 pl-4 m-0 box-border text-primary-text ${
-              title ? 'pt-3' : ''
+              title ? 'pt-4' : ''
             }`}
             value={String(value).replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
             onChange={(e) => handleChange(Number(e.target.value as string))}
           />
-          <input
-            className="absolute w-full max-w-[439px] inset-x-0 top-[43px] ml-4 cursor-pointer slider"
-            type="range"
-            min={min}
-            max={max}
-            step={step}
-            value={value}
-            onChange={(e) => handleChange(Number(e.target.value as number))}
-            style={getBackgroundSize()}
-          />
+          <div className="absolute inset-x-0 top-[34px] px-4">
+            <input
+              className="box-border w-full m-0 cursor-pointer slider"
+              type="range"
+              min={min}
+              max={max}
+              step={step}
+              value={value}
+              onChange={(e) => handleChange(Number(e.target.value as number))}
+              style={getBackgroundSize()}
+            />
+          </div>
         </div>
         <div className="flex justify-between mt-3 mb-4">
           {items.map((item, i) => (
