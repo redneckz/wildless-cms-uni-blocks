addd892257d27389b24824f00dcafd80004f2983
Игорь Бахтин <mikage345@gmail.com>
2022-07-06 19:37:29 +0500

    for lighthouse

--- docs/main.js
+++ docs/main.js
@@ -33661,6 +33661,12 @@ var IconMap;
 
 /* harmony default export */ const cosmos_decorator = (({ children }) => {
     document.querySelector(':root')?.setAttribute('lang', 'ru'); // for lighthouse accessibility metrics
+    if (!document.querySelector('meta[name=description]')) {
+        const meta = document.createElement('meta');
+        meta.name = 'description';
+        meta.content = 'description';
+        document.getElementsByTagName('head')[0].appendChild(meta);
+    }
     return (jsx("div", { style: {
             backgroundColor: 'rgba(51, 51, 51, 0.07)',
             position: 'absolute',
