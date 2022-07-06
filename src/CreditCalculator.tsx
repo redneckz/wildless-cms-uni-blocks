76006eba5e29c85c7f1f1612ee38a5e60fd3e86f
vladeiko <vladeiko999@gmail.com>
2022-07-06 15:01:21 +0300

    Add aria-label

--- src/CreditCalculator.tsx
+++ src/CreditCalculator.tsx
@@ -79,7 +79,10 @@ export const CreditCalculator = JSX<CreditCalculatorProps>(({ context, className
         </div>
         <div className="flex items-center">
           <Button className="mr-3" text="Отправить заявку" version="primary" href="#" />
-          <div className="w-80 text-xxs leading-4 text-secondary-text">
+          <div
+            className="w-80 text-xxs leading-4 text-secondary-text"
+            aria-label="Соглашение на обработку персональных данных"
+          >
             Отправляя заявку, вы соглашаетесь на обработку персональных данных соответственно
             требованиям ФЗ «О персональных данных»
           </div>
