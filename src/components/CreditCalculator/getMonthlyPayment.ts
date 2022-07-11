05da9257a063fa30ba83ca4945a6e0014769ae91
vladeiko <vladeiko999@gmail.com>
2022-07-11 12:46:01 +0300

    Fix monthly payment calculation

--- src/components/CreditCalculator/getMonthlyPayment.ts
+++ src/components/CreditCalculator/getMonthlyPayment.ts
@@ -25,7 +25,7 @@ export const getMonthlyPayment = (params: GetMonthlyPaymentParams) => {
 
   if (paymentType === 'annuity') {
     const annuityCoef = Number(rate) / 1200;
-    return finalSum * (annuityCoef + annuityCoef / (Math.pow(1 + annuityCoef, months - 1) - 1));
+    return finalSum * (annuityCoef + annuityCoef / (Math.pow(1 + annuityCoef, months) - 1));
   } else {
     return finalSum / months + finalSum * (Number(rate) / 12);
   }
