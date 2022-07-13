7411ae53368e1e8f4b61257e77b1f27821725858
Longraf <32570545+Longraf@users.noreply.github.com>
2022-07-13 15:50:31 +0300

    'feature/headline'

--- src/components/Headline/HeadlineContent.ts
+++ src/components/Headline/HeadlineContent.ts
@@ -1,12 +1,9 @@
 import type { BaseTileContent } from '../BaseTile/BaseTileContent';
-import { ContentPageContext } from '../ContentPage/ContentPageContext';
-
-export type HeadlineBgColor = 'transparent' | 'bg-white';
 
 export interface UniBlockProps {
   className?: string;
 }
 
 export interface HeadlineContent extends BaseTileContent {
-  bgColor?: HeadlineBgColor;
+  bgColor?: 'transparent' | 'bg-white';
 }
