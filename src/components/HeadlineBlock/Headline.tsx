2208bcf185c3571c120a6dd053e605dfbc33a6dd
Longraf <32570545+Longraf@users.noreply.github.com>
2022-07-13 12:15:03 +0300

    'feature/headline'

--- src/components/HeadlineBlock/Headline.tsx
+++ src/components/HeadlineBlock/Headline.tsx
@@ -1,12 +1,12 @@
 import { JSX } from '@redneckz/uni-jsx';
 
 import { BaseTile } from '../BaseTile/BaseTile';
-import type { HeadlineContent } from './HeadlineBlockContetnt';
+import type { HeadlineContent } from './HeadlineContetnt';
 import type { UniBlockProps } from '../../types';
 
 export interface HeadlineBlockProps extends UniBlockProps, HeadlineContent {}
 
-export const HeadlineBlock = JSX<HeadlineBlockProps>(({ bgColor, ...props }) => {
+export const Headline = JSX<HeadlineBlockProps>(({ bgColor, ...props }) => {
   return (
     <section className={`py-10 ${bgColor}`}>
       <BaseTile {...props}></BaseTile>
