7864b133f6ebdb505ef2fceba063c7f97a179065
Master Bloodoff <bludovigor@gmail.com>
2022-07-13 14:49:30 +0300

    Made changes in the code according to the comments in the github

--- src/components/PictureText/PictureTextContent.ts
+++ src/components/PictureText/PictureTextContent.ts
@@ -1,5 +1,5 @@
 import type { BaseTileContent } from '../BaseTile/BaseTileContent';
-import { Benefit } from '../BenefitsBlock/BenefitsBlockContent';
+import type { Benefit } from '../BenefitsBlock/BenefitsBlockContent';
 
 export interface PictureTextContent extends BaseTileContent {
   benefits?: Benefit[];
