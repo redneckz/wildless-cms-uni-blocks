2615ba63d98c23dca4f54394ae83c58433d53928
Andrey Simonenko <a.dev.krd@gmail.com>
2022-07-18 15:30:48 +0300

    Tile: add getContainerMinHeight

--- src/components/BaseTile/BaseTile.tsx
+++ src/components/BaseTile/BaseTile.tsx
@@ -48,7 +48,7 @@ export const BaseTile = JSX<BaseTileProps>(
             {title}
           </Title>
         )}
-        <div className="flex grow justify-between">
+        <div className="flex grow justify-between w-full">
           <div className={`flex flex-col justify-between ${alignBlock[align]}`}>
             <div>
               {description ? (
