3c3831c500040e4cb080d8a352085857c17b21ea
Master Bloodoff <bludovigor@gmail.com>
2022-07-19 15:17:51 +0300

    Fix design bugs in Footer

--- src/ui-kit/SearchBar.tsx
+++ src/ui-kit/SearchBar.tsx
@@ -4,6 +4,7 @@ import { Icon } from './Icon/Icon';
 
 export const SearchBar = JSX<UniBlockProps>(({ className, context }) => {
   const { term, setTerm } = context.useSearch();
+  const [inputHasFocus, setInputHasFocus] = context.useState<boolean>(false);
   return (
     <form className={`relative ${className || ''}`}>
       <div className="absolute rounded h-full flex items-center justify-center pl-4 max-w-[170px] gap-3.5 pointer-events-none">
@@ -12,7 +13,7 @@ export const SearchBar = JSX<UniBlockProps>(({ className, context }) => {
         </div>
         {!term && (
           <label
-            for="search-bar-input"
+            htmlFor="search-bar-input"
             className="font-sans font-normal text-base text-secondary-text"
           >
             Поиск по сайту
@@ -21,16 +22,21 @@ export const SearchBar = JSX<UniBlockProps>(({ className, context }) => {
       </div>
       <input
         id="search-bar-input"
-        className="pl-12 w-full font-sans font-normal text-base text-black border border-solid border-main-divider rounded h-full box-border outline-none pr-[6%]"
+        className={`${
+          inputHasFocus ? 'border-primary-text' : ''
+        } pr-32 h-12 pl-12 w-full font-sans font-normal text-base text-black border border-solid border-main-divider rounded h-full box-border outline-none pr-[6%]`}
         value={term}
         onChange={(e) => setTerm(e.target.value)}
+        onFocus={(e) => setInputHasFocus(true)}
+        onBlur={(e) => setInputHasFocus(false)}
         type="text"
-        name="s"
+        name="search-bar-input"
       />
-      {term && (
+      {inputHasFocus && (
         <button
           type="submit"
-          className="absolute font-sans font-normal text-sm cursor-pointer bg-transparent hover:bg-primary-hover active:bg-primary-active text-primary-main hover:text-white border-none px-5 py-2 rounded top-1 right-1 outline-none"
+          version="primary"
+          className="absolute font-sans font-normal cursor-pointer text-sm hover:bg-primary-hover active:bg-primary-active border-none px-9 py-[9px] rounded top-1 right-1 outline-none text-secondary-text text-white bg-primary-main"
           onClick={(e) => {
             e.preventDefault();
             console.log('click');
