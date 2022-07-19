c7d31c0bbc6f0fc5ba65163f748446869dbc3dbc
Alexander Alexandrov <vasyaselsky@yandex.ru>
2022-07-19 11:22:22 +0300

    Configure code quality metrics (#96)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,18 +6,35 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-types': 'warn',
-    'local-eslint-rules/consistent-blocks-registry': [
-      'warn',
-      {
-        blocksRegistry: 'src/components/Blocks.ts',
-        blocksDir: 'src/components',
-        exclude: [/^.*Page$/, /^.*Control$/, /^Base.*$/, /^.*Content$/, /^.*Blocks$/],
-      },
-      {
-        blocksRegistry: 'src/components/MobileBlocks.ts',
-        blocksDir: 'src/components',
-        include: [/^.*\.mobile$/],
-      },
-    ],
+    camelcase: ['error', { allow: ['node_ids', 'node_id', 'user_name'] }],
   },
+  overrides: [
+    {
+      files: ['src/**/*'],
+      excludedFiles: ['src/**/*.fixture.tsx', 'src/**/*.spec.tsx', 'src/**/*.spec.ts'],
+      rules: {
+        complexity: ['warn', 7],
+        'max-lines': ['error', { max: 150, skipBlankLines: true, skipComments: true }],
+        'max-depth': ['error', 2],
+        'max-params': ['error', 3],
+        'max-statements-per-line': ['error', { max: 1 }],
+        'max-nested-callbacks': ['warn', 3],
+        'max-lines-per-function': ['warn', { max: 50, skipComments: true }],
+        'no-multiple-empty-lines': ['error', { max: 1 }],
+        'local-eslint-rules/consistent-blocks-registry': [
+          'warn',
+          {
+            blocksRegistry: 'src/components/Blocks.ts',
+            blocksDir: 'src/components',
+            exclude: [/^.*Page$/, /^.*Control$/, /^Base.*$/, /^.*Content$/, /^.*Blocks$/],
+          },
+          {
+            blocksRegistry: 'src/components/MobileBlocks.ts',
+            blocksDir: 'src/components',
+            include: [/^.*\.mobile$/],
+          },
+        ],
+      },
+    },
+  ],
 };
