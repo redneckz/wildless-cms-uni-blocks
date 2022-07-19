4c14928aa9b6691930fcb8ff51ed5fa9da5271b4
Andrey Malenkov <grimmreaper13@rambler.ru>
2022-07-20 02:10:25 +0300

    StepsBlock style fixes (#167)

--- src/components/StepsBlock/StepsBlock.fixture.tsx
+++ src/components/StepsBlock/StepsBlock.fixture.tsx
@@ -1,23 +1,48 @@
 import { context } from '../../setup-fixture';
 import { StepsBlock } from './StepsBlock';
+import type { Step } from './StepsBlockContent';
+import { IconName } from '../../ui-kit/Icon/IconProps';
 
-export default (
-  <StepsBlock
-    context={context}
-    title="Рассчитайте условия на онлайн-калькуляторе и оставьте заявку на потребительский кредит"
-    steps={[
-      {
-        label: 'Заявка на кредит',
-        description: 'Оставьте онлайн-заявку на потребительский кредит',
-      },
-      {
-        label: 'В случае одобрения',
-        description: 'Менеджер свяжется с вами и согласует встречу',
-      },
-      {
-        label: 'Получение наличных',
-        description: 'Получите наличные в отделении банка',
-      },
-    ]}
-  />
-);
+const title =
+  'Рассчитайте условия на онлайн-калькуляторе и оставьте заявку на потребительский кредит';
+
+const steps: Step[] = [
+  {
+    label: 'Заявка на кредит',
+    description: 'Оставьте онлайн-заявку\nна потребительский кредит',
+  },
+  {
+    label: 'В случае одобрения',
+    description: 'Менеджер свяжется с вами\nи согласует встречу',
+  },
+  {
+    label: 'Получение наличных',
+    description: 'Получите наличные\nв отделении банка',
+  },
+];
+
+const hybridStepHeadings: Step[] = [
+  {
+    label: 'Заявка на кредит',
+    description: 'Оставьте онлайн-заявку\nна потребительский кредит',
+  },
+  {
+    label: 'В случае одобрения',
+  },
+  {
+    description: 'Получите наличные\nв отделении банка',
+  },
+];
+
+const icons: IconName[] = ['MoneyIcon', 'UserIcon', 'DocIcon'];
+
+const iconSteps: Step[] = steps.map((item, i) => ({ ...item, icon: icons[i] }));
+
+export default {
+  default: <StepsBlock context={context} title={title} steps={steps} />,
+  '2 steps': <StepsBlock context={context} title={title} steps={steps.slice(0, 2)} />,
+  'no lines': <StepsBlock context={context} title={title} steps={steps} showLines={false} />,
+  'hybrid step headings': <StepsBlock context={context} title={title} steps={hybridStepHeadings} />,
+  'with icons': <StepsBlock context={context} title={title} steps={iconSteps} />,
+  'no heading': <StepsBlock context={context} steps={steps} />,
+};
