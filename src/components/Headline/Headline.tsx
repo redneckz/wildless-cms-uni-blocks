64201ea478f24359d96843adcc3fb7db73dbfb80
Longraf <32570545+Longraf@users.noreply.github.com>
2022-07-21 13:47:27 +0300

    headline refactor (#109)
    * headline refactor
    
    * fix
    
    * return asSVG
    
    * return asSVG
    
    * fix
    
    Co-authored-by: Igor Bakhtin <Mikage777@users.noreply.github.com>
    Co-authored-by: Игорь Бахтин <mikage345@gmail.com>

--- src/components/Headline/Headline.tsx
+++ src/components/Headline/Headline.tsx
@@ -1,15 +1,32 @@
 import { JSX } from '@redneckz/uni-jsx';
 
 import type { UniBlockProps } from '../../types';
-import { BaseTile } from '../BaseTile/BaseTile';
+import { Title } from '../../ui-kit/Title/Title';
 import type { HeadlineContent } from './HeadlineContent';
+import { AlignType } from '../BaseTile/BaseTileProps';
 
 export interface HeadlineProps extends UniBlockProps, HeadlineContent {}
 
-export const Headline = JSX<HeadlineProps>(({ bgColor = 'bg-white', className, ...props }) => {
-  return (
-    <section className={`p-10 ${bgColor} ${className || ''} `}>
-      <BaseTile {...props}></BaseTile>
-    </section>
-  );
-});
+const alignText: Record<AlignType, string> = {
+  left: 'text-left',
+  center: 'text-center',
+  right: 'text-right',
+};
+
+export const Headline = JSX<HeadlineProps>(
+  ({ bgColor = 'transparent', align = 'left', className, title, description }) => {
+    const textClasses = alignText[align];
+    return (
+      <section className={`p-[50px] ${bgColor} ${className || ''} `}>
+        {title && (
+          <Title size="L" className={`text-primary-text mt-0 font-medium ${textClasses}`}>
+            {title}
+          </Title>
+        )}
+        {description && (
+          <p className={`font-normal text-base mt-4 ${textClasses}`}>{description}</p>
+        )}
+      </section>
+    );
+  },
+);
